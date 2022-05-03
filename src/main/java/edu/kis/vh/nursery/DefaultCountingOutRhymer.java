c5e97f4eb314ef0f2a8bee63c49ebbc65b179c22
Name <Email>
2022-05-03 21:28:16 +0200

    Rozdzielenie stalej _TOTAL na stala _EMPTY_ARRAY_INDICATOR oraz _EMPTY_ARRAY_VALUE

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,15 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private static final int _TOTAL = -1;
-
 	private static final int _ARRAY_SIZE = 12;
-
     private static final int _MAX_ARRAY_INDEX = _ARRAY_SIZE - 1;
+    private static final int _EMPTY_ARRAY_INDICATOR = -1;
+    private static final int _EMPTY_ARRAY_VALUE = -1;
 
 	private int[] numbers = new int[_ARRAY_SIZE];
-
-    public int total = _TOTAL;
+    public int total = _EMPTY_ARRAY_INDICATOR;
 
     public void countIn(int in) {
         if (!isFull())
@@ -18,7 +16,7 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == _TOTAL;
+        return total == _EMPTY_ARRAY_INDICATOR;
     }
 
     public boolean isFull() {
@@ -27,13 +25,13 @@ public class DefaultCountingOutRhymer {
 
     protected int peekaboo() {
         if (callCheck())
-            return _TOTAL;
+            return _EMPTY_ARRAY_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return _TOTAL;
+            return _EMPTY_ARRAY_VALUE;
         return numbers[total--];
     }
 
